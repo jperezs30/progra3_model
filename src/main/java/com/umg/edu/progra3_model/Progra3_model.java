795a6d91c429dc07b2e5609c0b58981740c2f3cb
Fernando Sipaque <josefperez84@gmail.com>
2025-05-23 22:01:26 -0600

    Initializes the project structure
    Sets up the basic Maven project structure with a simple "Hello World" application.
    
    This commit creates the pom.xml file and a basic Java class to
    establish the initial project setup.

---
+++ src/main/java/com/umg/edu/progra3_model/Progra3_model.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.umg.edu.progra3_model;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Progra3_model {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
